fed21d875e027b5c8afcbf836d549f54cd30ff63
Jeff <Jeff>
2016-12-08 17:05:58 -0500

    class 12.7.16

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+var questions = [
+	{audio : ,
+	guesses : ["Janna", "Vi", "Ahri",
+				"Fiora", "Lissandra"],
+	answer: 3}, //Fiora
+	{audio : ,
+	guesses : ["Thresh", "Garen", "Hecarim",
+				"Kallista", "Darius"],
+	answer : 2}, //Hecarim
+	{audio : ,
+	guesses : ["Jhin", "Teemo", "Yasuo", 
+				"Draven", "Mordekaiser"],
+	answer : 0}, //Jhin
+	{audio : ,
+	guesses : ["Sona", "Lux", "Leblanc",
+				"Ashe", "Kindred"],
+	answer : 4}, //Kindred
+	{audio : ,
+	guesses : [""]}, //Rammus
+
+]
