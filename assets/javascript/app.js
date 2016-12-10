3745e07d80e239ce6518362192de20f1e5b19a52
blupie01 <jeffrey.chou.89@gmail.com>
2016-12-10 01:17:28 -0500

    12.10.16

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 //variable for questions and answers
 var questions = [
-	{audio : "assets/audio/FioraPROJECT.taunt01.ogg'",
+	{audio : "assets/audio/FioraPROJECT.taunt01.ogg",
 	guesses : ["Janna", "Vi", "Ahri",
 				"Fiora", "Lissandra"],
 	answer: 3}, //Fiora
@@ -35,10 +35,10 @@ var questions = [
 ];
 
 // other variables
-var timer = 30; //set number counter to 30
-var counter; // var to hold set interval when countDown() is executed
 var ans_correct = 0;
 var ans_wrong = 0;
+var question_Index = 0;
+var counter;
 
 //hide all elements except for start button
 $(document).ready(function () {
@@ -46,7 +46,6 @@ $(document).ready(function () {
 	$("#heroVoice").hide();
 	$("#heroChoices").hide();
 	$("#noTime").hide();
-	$("#submitButton").hide();
 });
 
 //function to hide start button and begin game
@@ -54,101 +53,91 @@ $("#startButton").on("click", function(){
 	$("#timer").show();
 	$("#heroVoice").show();
 	$("#heroChoices").show();
-	$("#submitButton").show();
 
 	startGame();
 });
 
-//function to set count down INTERVAL by one second
-function countDown() {
-	counter = setInterval(decrement, 1000);
-}
-
-//clears counter interval
-function stop() {
-	clearInterval(counter);
-}
-
-//function to put count down on screen and call function countDown
-//to set interval
-function decrement() {
-	//decrease timer by one
-	timer--;
-
-	//show time left for question in #timer tag
-	$("#timer").html("<p>Time Remainging: " + timer + "</p>");
+//function to add audio clip for question
+function addAudio (audioFile){
+	//appending audio tag to #heroVoice
+	// var audio_link = questions[audioIndex].audio;
+	var audio = $("<audio controls='controls' autoplay></audio>").attr(
+					"preload", "metadata").attr("style", "width: 500px;").attr(
+					"src", audioFile);
+	$('#heroVoice').html(audio);
 }
 
 //function to make answer buttons in #heroChoices
-function makeHeroChoices(questionsIndex) {
-	var heroList = questions[questionsIndex].guesses;
-	console.log(heroList);
-	for (var i = 0; i < heroList.length; i++) {
-		$("#heroChoices").append("<button>" + heroList[i] + "</button");
-		
-		if(i == heroList.length-1) {
-			console.log("here");
-			$("#heroChoices").append("<button>Submit</button>");
-		}
+function makeHeroChoices(heroArray) {
+	for (var i = 0; i < heroArray.length; i++) {
+		//create button for hero name
+		var button = $("<button id='heroButton'>" + heroArray[i] + "</button>");
+		//add data to button
+		button.attr("hero_value", i);
+		//add button to screen
+		$("#heroChoices").append(button);
 	}
+}
+
+//function to reload
+function reset() {
+	location.reload();
 }
 
 //function for game rules and operations
 function startGame() {
 	$("#startButton").hide();
 
-	countDown();
+	//time per question
+	var timer = 30;
+	//get question at current index
+	var currentAudio = questions[question_Index].audio;
+	//get hero choices array at current index
+	var heroChoices = questions[question_Index].guesses;
+	//get answer at current index
+	var answer = questions[question_Index].answer;
+	//put time on screen
+	var displayTimer = $("#timer").html("<p>Time Remaining: " + timer + "</p>");
+	//set counter variable
+	counter = setInterval(thirtySecondTimer, 1000);
 
-	//loop through questions array and put to screen
-	for (var i = 0; i < questions.length; i++){
-		console.log("first for loop");
-		console.log(questions[i]);
-
-		makeHeroChoices(i);
-
-		$("")
-
-		if (timer === 0) {
-			console.log("here");
-			stop();
-
-			// if (i == questions.length-1) {
-
-			// }
+	//function for thirty second countdown
+	function thirtySecondTimer() {
+		//if clock reaches 0
+		if (timer == 0) {
+			clearTimeout(counter);
+			$("#heroVoice").empty();
+			$("#heroChoices").html("No Time Left");
+			setTimeout(reset, 5000);
+		}
+		else {
+			displayTimer.html("<p>Time Remaining: " + timer + "</p>");
+			timer--;
 		}
 	}
+
+	//call function to put audio clip on screen
+	addAudio(currentAudio);
+
+	//call function to put choices on screen
+	makeHeroChoices(heroChoices);
+
+	$("#heroButton").on("click", function() {
+		var selectedAnswer = $(this).attr("hero_value");
+
+		if (selectedAnswer == answer) {
+			question_Index++;
+			ans_correct++;
+			clearTimeout(counter);
+			$("#heroChoices").html("<p>Correct!</p>");
+			startGame();
+		}
+		else if (selectedAnswer != answer) {
+			question_Index++;
+			ans_wrong++;
+			clearTimeout(counter);
+			$("#heroChoices").html("<p>Wrong!</p>");
+			startGame();
+		}
+	});
 }
-
-    //  The decrement function.
-    // function decrement() {
-
-      //  Decrease number by one.
-      // number--;
-
-      //  Show the number in the #show-number tag.
-      // $("#show-number").append("<h2>" + number + "</h2>");
-
-
-      //  Once number hits zero...
-      // if (number === 0) {
-
-        //  ...run the stop function.
-        // stop();
-
-        //  Alert the user that time is up.
-    //     alert("Time Up!");
-    //   }
-    // }
-
-
-
-function add_audio (test){
-	//appending audio tag to #heroVoice
-	var audio_link = "'audio/Thresh.tauntVoidborn01.ogg'";
-	var audio = $("<audio controls='controls' autoplay></audio>").attr(
-					"preload", "metadata").attr("style", "width: 500px;").attr(
-					"src", "assets/audio/ThreshDarkStar.tauntVoidborn01.ogg");
-	$('#heroVoice').append(audio);
-}
-
-// add_audio();
